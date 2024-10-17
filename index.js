98e0bfb6d439165a167b95c23399d503f15c0f85
Pawit Thongkum <96004821+PerthTheIamCat@users.noreply.github.com>
2024-10-17 06:11:45 +0000

    add : connect database

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mysql = require('mysql')
 const app = express()
 const port = 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
+
+const db = mysql.createConnection({
+  host: 'korawit.ddns.net',
+  user: 'webapp',
+  password: 'secret2024', 
+  port: 3307,
+  database: 'shop'
+});
+db.connect((err) => {
+  if (err) {
+    console.error('ไม่สามารถเชื่อมต่อกับฐานข้อมูลได้: ', err);
+  } else {
+    console.log('เชื่อมต่อฐานข้อมูลสำเร็จ');
+  }
+});
  
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","*");
@@ -33,11 +49,26 @@ app.get('/', (req, res) => {
 })
  
 app.get('/api/products',(req,res)=>{
-  if(products.length>0)
-    res.send(products);
-  else
-    res.status(400).send("No products founds");
+  db.query('SELECT * FROM products', (err, results) => {
+    if (err) {
+      return res.status(500).send('เกิดข้อผิดพลาดกับฐานข้อมูล');
+    } else {
+      return res.json(results);
+    }
+  });
 })
+app.get('/api/products/:id', (req, res) => {
+  const productId = req.params.id;
+  db.query('SELECT * FROM products WHERE id = '+productId, (err, result) => {
+    if (err) {
+      return res.status(500).send('Error querying the database');
+    }
+    if (result.length === 0) {
+      return res.status(404).send('Product not found');
+    }
+    res.json(result);
+  });
+});
  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
